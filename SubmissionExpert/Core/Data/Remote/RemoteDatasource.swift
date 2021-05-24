1d8212d551630737a95ff8922e2a90df53942839
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-05-24 13:48:45 +0700

    Resolve Warning

--- SubmissionExpert/Core/Data/Remote/RemoteDatasource.swift
+++ SubmissionExpert/Core/Data/Remote/RemoteDatasource.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 import Alamofire
 
-protocol RemoteDatasourceProtocol: class {
+protocol RemoteDatasourceProtocol: AnyObject {
     func getRandomMenu()->AnyPublisher<[RandomMenuResponse], Error>
 }
 
--- SubmissionExpert/Module/Home/Presenter/HomePresenter.swift
+++ SubmissionExpert/Module/Home/Presenter/HomePresenter.swift
@@ -9,31 +9,14 @@ import Foundation
 import Combine
 
 class HomePresenter: ObservableObject {
-    private var cancellables: Set<AnyCancellable> = []
   
     private let useCase: HomeUseCase
     
     init(useCase: HomeUseCase) {
         self.useCase = useCase
     }
-    
-    @Published var randomMenu:[RandomMenuResponse] = []
-    @Published var errorMessage: String = ""
-    @Published var loadingState: Bool = false
-    
-    func getCategories() {
-        loadingState = true
-        useCase.getRandomMenu().receive(on: RunLoop.main).sink { (completion) in
-            switch completion {
-            
-            case .finished:
-                self.loadingState = false
-            case .failure(_):
-                self.errorMessage = String(describing: completion)
-            }
-        } receiveValue: { (result) in
-            self.randomMenu = result
-        }.store(in: &cancellables)
 
+    func getCategories() -> AnyPublisher<[RandomMenuResponse], Error > {
+        return useCase.getRandomMenu()
     }
 }
